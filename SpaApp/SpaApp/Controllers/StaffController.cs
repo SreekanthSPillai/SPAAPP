b5b9bd36e558f8a8ae2cdcdf6cf14e1d1ba5c261
SreekanthSPillai <sreeacl@yahoo.com>
2015-01-30 16:53:22 +0000

    SPA Backbone base

---
+++ SpaApp/SpaApp/Controllers/StaffController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpaDemo.Controllers
+{
+    public class StaffController : Controller
+    {
+        //
+        // GET: /Todo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
